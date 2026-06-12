616c621bde3dce52455aa06fb3b00169628688d9
matteovecchio04 <matteovex@gmail.com>
2026-06-12 02:11:54 +0200

    Refactor App component to fetch and display videos with updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3360/api/videos")
+      .then((res) => res.json())
+      .then((data) => setVideos(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
-      <h1>Believe Play</h1>
+
+      <div className="app">
+        <h1 className="title">BelievePlay</h1>
+
+        <div className="grid">
+          {videos.map(video => (
+            <div className="card" key={video.id}>
+              <img className="card-img" src={video.thumbnail} />
+              <p className="card-title">{video.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
     </>
   )
 }
